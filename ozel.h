da4ae8572ac1b44352050020372ce70c520017d9
Yusuf <yusufsurun@proton.me>
2024-08-27 00:09:12 +0300

    Add files via upload

---
+++ ozel.h
@@ -0,0 +1,60 @@
+//@ysfcdr (GitHub)
+#include <iostream>
+using namespace std;
+
+//Ç
+int kce = 135;
+int bce = 128;
+//Ö
+int koe = 148;
+int boe = 153;
+//Ü
+int kue = 129;
+int bue = 154;
+//Ş
+int kse = 159;
+int bse = 158;
+//İ
+int bie = 176;
+
+//ç
+int kc()
+{
+    return kce;
+}
+//Ç
+int bc(){
+    return(bce);
+}
+
+//ö
+int ko(){
+    return koe;
+}
+//Ö
+int bo(){
+    return boe;
+}
+
+//ü
+int ku(){
+    return kue;
+}
+//Ü
+int bu(){
+    return bue;
+}
+
+//ş
+int ks(){
+    return kse;
+}
+//Ş
+int bs(){
+    return bse;
+}
+
+//İ
+int bi(){
+    return bie;
+}
